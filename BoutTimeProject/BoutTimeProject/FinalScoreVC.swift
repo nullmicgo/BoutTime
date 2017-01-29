92c3b388e134f874693af235860ec646bfd97342
michael <nullmicgo@gmail.com>
2017-01-30 02:37:07 +0800

    updated the UI for the selection items

---
+++ BoutTimeProject/BoutTimeProject/FinalScoreVC.swift
@@ -0,0 +1,25 @@
+//
+//  FinalScoreVC.swift
+//  BoutTimeProject
+//
+//  Created by michael on 27/1/2017.
+//  Copyright © 2017 michael. All rights reserved.
+//
+
+import UIKit
+
+class FinalScoreVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
